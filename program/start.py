7682a2d4338fc0cff5d0b476baecc6cbb63b912e
USDDBOT <101028273+USDDBOT@users.noreply.github.com>
2022-03-07 13:30:22 +0200

    Add files via upload

--- program/start.py
+++ program/start.py
@@ -115,7 +115,7 @@ async def alive(client: Client, message: Message):
                 ),
             ],
             [
-                InlineKeyboardButton("♡اضف البوت الى مجموعتك♡", url=f"https://t.me/USDDBOT?startgroup=true"),
+                InlineKeyboardButton("♡اضف البوت الى مجموعتك♡", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
             ]
         ]
     )
@@ -195,3 +195,20 @@ async def new_chat(c: Client, m: Message):
 
 
 chat_watcher_group = 5
+
+@Client.on_message(group=chat_watcher_group)
+async def chat_watcher_func(_, message: Message):
+    try:
+        userid = message.from_user.id
+    except Exception:
+        return
+    suspect = f"[{message.from_user.first_name}](tg://user?id={message.from_user.id})"
+    if await is_gbanned_user(userid):
+        try:
+            await message.chat.ban_member(userid)
+        except Exception:
+            return
+        await message.reply_text(
+            f"👮🏼 (> {suspect} <)\n\n**Gbanned** user detected, that user has been gbanned by sudo user and was blocked from this Chat !\n\n🚫 **Reason:** potential spammer and abuser."
+        )
+
